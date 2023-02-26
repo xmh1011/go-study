ce807009b0198d64a7262548e968453226ea76cc
xmh1011 <1197843839@qq.com>
2023-02-26 22:13:23 +0800

    整点浪漫

--- chapter_1/简单web服务器框架.go
+++ chapter_1/简单web服务器框架.go
@@ -7,14 +7,10 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", index)                           // index 为向 url发送请求时，调用的函数
+	http.HandleFunc("/", index)                         // index 为向 url发送请求时，调用的函数
 	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil)) // ListenAndServe 监听端口，如果端口被占用，会报错
 }
 
 func index(w http.ResponseWriter, r *http.Request) { // index 函数接收两个参数，第一个是 http.ResponseWriter，第二个是 *http.Request
-	fmt.Fprintf(w, "vv不要再生气了！vv是个大美女！\n")
-	fmt.Fprintf(w," ╭︿︿︿╮\n")
-	fmt.Fprintf(w,"{/ @  @ /}\n")
-	fmt.Fprintf(w," ( (oo) ) \n")
-	fmt.Fprintf(w,"  ︶︶︶ \n")
+	fmt.Fprintf(w, "你在干嘛？\n")
 }

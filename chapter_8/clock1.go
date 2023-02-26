b9cc1443fd444678b597d6e6199222aa81428d9e
xmh1011 <1197843839@qq.com>
2023-02-26 22:10:39 +0800

    chapter_8 Concurrent Programming

---
+++ chapter_8/clock1.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+// 如何使用该程序？
+// 1. 在命令行中，进入本程序所在的目录
+// 2. 执行 go build clock1.go 命令，生成 clock1 程序
+// 3. 执行 ./clock1 命令，启动 clock1 程序
+// 4. 打开另一个命令行窗口，执行 nc localhost 8000 命令，连接到 clock1 程序
+
+// 本程序的功能是，每隔一秒，向客户端发送当前时间
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000") // 创建一个监听器，监听端口 8000
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		// conn 是一个 net.Conn 类型的变量，用于表示客户端的连接
+		conn, err := listener.Accept() // 接收客户端的连接请求
+		if err != nil {
+			// 如果接收客户端的连接请求失败，则打印错误信息，然后继续接收下一个客户端的连接请求
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn) // 并发处理客户端的连接请求
+	}
+}
+
+// handleConn 函数接收一个 net.Conn 类型的参数，然后向客户端发送当前时间
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n")) // 向客户端发送当前时间
+		if err != nil {
+			return
+		}
+		time.Sleep(1 * time.Second) // 每隔一秒，向客户端发送当前时间
+	}
+}
---
+++ chapter_8/netcat1.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+// 如何使用该程序？
+// 1. 在命令行中，进入本程序所在的目录
+// 2. 执行 go build netcat1.go 命令，生成 netcat1 程序
+// 3. 执行 ./netcat1 命令，启动 netcat1 程序
+// 4. 打开另一个命令行窗口，执行 nc localhost 8000 命令，连接到 netcat1 程序
+// 5. 在第 4 步中打开的命令行窗口中，输入一些文本，然后按下回车键，可以看到这些文本被打印到了第 3 步中打开的命令行窗口中
+
+func main() {
+	// conn 是一个 net.Conn 类型的变量，用于表示客户端的连接
+	conn, err := net.Dial("tcp", "localhost:8000") // 创建一个连接，连接到端口 8000
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()        // 关闭连接
+	mustCopy(os.Stdout, conn) // 从标准输入中读取数据，然后写入到连接中
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// mustCopy是一个io.Copy的包装器，如果io.Copy返回一个错误，那么就会调用log.Fatal函数，终止程序的执行
